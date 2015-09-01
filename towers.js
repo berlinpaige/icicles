f0dbc6b2764d942a05d1643fd93f8655eb10ad44
Berlin Sohn <berlinsohn@gmail.com>
2015-09-01 10:22:42 -0700

    add refresh button

--- towers.js
+++ towers.js
@@ -23,21 +23,25 @@ var towers = {
          towers.hasClass0 = true;
          $('#size0').addClass('selected');
          towers.selectedElement = $('#size0').clone();
+         towers.class0 = $('#size0').clone();
          towers.elementSelected = true;
       }else if ($(this).children().children().hasClass('1')){
          towers.hasClass1 = true;
          $('#size1').addClass('selected');
          towers.selectedElement = $('#size1').clone();
+         towers.class1 = $('#size1').clone();
          towers.elementSelected = true;
       }else if ($(this).children().children().hasClass('2')){
          towers.hasClass2 = true;
          $('#size2').addClass('selected');
          towers.selectedElement = $('#size2').clone();
+         towers.class2 = $('#size2').clone();
          towers.elementSelected = true;
       }else if ($(this).children().children().hasClass('3')){
          towers.hasClass2 = true;
          $('#size3').addClass('selected');
          towers.selectedElement = $('#size3').clone();
+         towers.class3 = $('#size3').clone();
          towers.elementSelected = true;
       }
     }else{
@@ -49,8 +53,7 @@ var towers = {
     if ($('#rightContainer').children().children().hasClass('0') && $('#rightContainer').children().children().hasClass('1') && $('#rightContainer').children().children().hasClass('2') && $('#rightContainer').children().children().hasClass('3')){
       console.log('gameOver!');
       towers.gameOver();
-    } 
-     
+    }   
     if (towers.elementSelected === true){
      if (towers.selectedElement.hasClass('0')){
         $('#size0').remove();
@@ -73,19 +76,8 @@ var towers = {
     $('.backdrops').attr('disabled', 'disabled');
   },
   newGameIsClicked: function(){
-    hasClass0 = false;
-    hasClass1 = false;
-    hasClass2 = false;
-    hasClass3 = false;
-    selectedElement = -1;
-    elementSelected = false;
     $('.backdrops').removeAttr('disabled');
-    $('#leftContainer').append($('#size3'));
-    $('#leftContainer').append($('#size2'));
-    $('#leftContainer').append($('#size1'));
-    $('#leftContainer').append($('#size0'));
-
-
+    document.location.reload();
   }
 }
 
