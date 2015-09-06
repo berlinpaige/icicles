cdb6adec59ec1b5ff3a1bedf5ce11ad6b0f85ed5
Berlin Sohn <berlinsohn@gmail.com>
2015-09-06 11:18:23 -0700

    add drag and drop feature

--- towers.js
+++ towers.js
@@ -6,70 +6,64 @@ var towers = {
   hasClass2: false,
   hasClass3: false,
   selectedElement: -1,
-  elementSelected: false,
-
+  
   init: function(){
-    $('.backdrops').dblclick(towers.selectBox); 
-    $('.backdrops').click(towers.placeBox); 
+    $('.backdrops').mousedown(towers.selectBox);
+    $('.backdrops').mousedown(towers.draggable);
     $('button').click(towers.newGameIsClicked);
+    towers.droppable();
+  },
+
+  draggable: function(){
+    $('.blocks').draggable({
+      helper: 'clone'
+    });
+  },
+  droppable: function(){
+    $('.backdrops').droppable({
+      drop: function(event, ui){ 
+         if (towers.selectedElement.hasClass('0')){
+            $('#size0').remove();
+            $(this).children().append(towers.selectedElement);
+          }else if (towers.selectedElement.hasClass('1') && $(this).children().children().hasClass('0') === false){
+            $('#size1').remove();
+            $(this).children().append(towers.selectedElement);
+          }else if(towers.selectedElement.hasClass('2') && $(this).children().children().hasClass('0') === false && $(this).children().children().hasClass('1') === false){
+            $('#size2').remove();
+            $(this).children().append(towers.selectedElement);
+          }else if(towers.selectedElement.hasClass('3') && $(this).children().children().hasClass('0') === false && $(this).children().children().hasClass('1') === false && $(this).children().children().hasClass('2') === false){
+            $('#size3').remove();
+            $(this).children().append(towers.selectedElement);
+          }  
+      towers.isGameOver();
+      }
+    });
+  },
+  isGameOver: function(){
+    if ($('#rightContainer').children().children().hasClass('0') && $('#rightContainer').children().children().hasClass('1') && $('#rightContainer').children().children().hasClass('2') && $('#rightContainer').children().children().hasClass('3')){
+        console.log('gameOver!');
+        towers.gameOver();
+      } 
   },
   selectBox: function(){
-    if (towers.elementSelected === false){
-      towers.hasClass0 = false;
-      towers.hasClass1 = false;
-      towers.hasClass2 = false;
-      towers.hasClass3 = false;
-      if($(this).children().children().hasClass('0')){
-         towers.hasClass0 = true;
-         $('#size0').addClass('selected');
-         towers.selectedElement = $('#size0').clone();
-         towers.class0 = $('#size0').clone();
-         towers.elementSelected = true;
-      }else if ($(this).children().children().hasClass('1')){
-         towers.hasClass1 = true;
-         $('#size1').addClass('selected');
-         towers.selectedElement = $('#size1').clone();
-         towers.class1 = $('#size1').clone();
-         towers.elementSelected = true;
-      }else if ($(this).children().children().hasClass('2')){
-         towers.hasClass2 = true;
-         $('#size2').addClass('selected');
-         towers.selectedElement = $('#size2').clone();
-         towers.class2 = $('#size2').clone();
-         towers.elementSelected = true;
-      }else if ($(this).children().children().hasClass('3')){
-         towers.hasClass2 = true;
-         $('#size3').addClass('selected');
-         towers.selectedElement = $('#size3').clone();
-         towers.class3 = $('#size3').clone();
-         towers.elementSelected = true;
-      }
-    }else{
-      $('.backdrops').siblings().children().children().removeClass('selected');
-      towers.elementSelected = false
+    if($(this).children().children().hasClass('0')){
+       towers.hasClass0 = true;
+       towers.selectedElement = $('#size0').clone();
+       towers.class0 = $('#size0').clone();
+    }else if ($(this).children().children().hasClass('1')){
+       towers.hasClass1 = true;
+       towers.selectedElement = $('#size1').clone();
+       towers.class1 = $('#size1').clone();
+    }else if ($(this).children().children().hasClass('2')){
+       towers.hasClass2 = true;
+       towers.selectedElement = $('#size2').clone();
+       towers.class2 = $('#size2').clone();
+    }else if ($(this).children().children().hasClass('3')){
+       towers.hasClass2 = true;
+       towers.selectedElement = $('#size3').clone();
+       towers.class3 = $('#size3').clone();
     }
-  },
-  placeBox: function(){
-    if ($('#rightContainer').children().children().hasClass('0') && $('#rightContainer').children().children().hasClass('1') && $('#rightContainer').children().children().hasClass('2') && $('#rightContainer').children().children().hasClass('3')){
-      console.log('gameOver!');
-      towers.gameOver();
-    }   
-    if (towers.elementSelected === true){
-     if (towers.selectedElement.hasClass('0')){
-        $('#size0').remove();
-        $(this).children().append(towers.selectedElement);
-      }else if (towers.selectedElement.hasClass('1') && $(this).children().children().hasClass('0') === false){
-        $('#size1').remove();
-        $(this).children().append(towers.selectedElement);
-      }else if(towers.selectedElement.hasClass('2') && $(this).children().children().hasClass('0') === false && $(this).children().children().hasClass('1') === false){
-        $('#size2').remove();
-        $(this).children().append(towers.selectedElement);
-      }else if(towers.selectedElement.hasClass('3') && $(this).children().children().hasClass('0') === false && $(this).children().children().hasClass('1') === false && $(this).children().children().hasClass('2') === false){
-        $('#size3').remove();
-        $(this).children().append(towers.selectedElement);
-      }  
-    }
-  },
+  }, 
   gameOver: function(){
     $('h1, h4, p').text('');
     $('h1').text("YOU WON!!  Let's Play Again");
